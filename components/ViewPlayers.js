1631d017ec05a268c61631823bfe38c859b121cb
KelsCree <kelseycreehan@gmail.com>
2021-02-13 15:37:47 -0700

    Add styling to players.

--- components/ViewPlayers.js
+++ components/ViewPlayers.js
@@ -9,13 +9,14 @@ const ViewPlayers = ({ players }) => {
   const Item = ({ item }) => (
     <TouchableOpacity
       key={item.id}
+      style={styles.item}
       onPress={() => selectPlayer(item)}
     >
-      <Text>{item.name}</Text>
-      <Text>{item.gender}</Text>
-      <Text>{item.age}</Text>
-      <Text>{item.state}</Text>
-      <Text>{item.satus}</Text>
+      <Text style={styles.name}>{item.name}</Text>
+      <Text style={styles.info}>Gender: {item.gender}</Text>
+      <Text style={styles.info}>Age: {item.age}</Text>
+      <Text style={styles.info}>State: {item.state}</Text>
+      <Text style={styles.info}>Status: {item.status}</Text>
     </TouchableOpacity>
   )
 
@@ -31,21 +32,59 @@ const ViewPlayers = ({ players }) => {
 
 
   return (
-    <SafeAreaView>
-      <Text>Players</Text>
-        <FlatList
-          title={"Players"}
-          data={players}
-          renderItem={renderItem}
-          keyExtractor={(item) => {
-            return item.id;
-          }}
-        />
-
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Players</Text>
+      <SafeAreaView style={styles.listContainer}>
+          <FlatList
+            style={styles.list}
+            data={players}
+            renderItem={renderItem}
+            keyExtractor={(item) => {
+              return item.id;
+            }}
+          />
+      </SafeAreaView>
+    </View>
   )
 }
 
-
-
 export default ViewPlayers
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10
+  },
+  item: {
+    borderRadius: 8,
+    backgroundColor: '#FFFFFF',
+    padding: 15,
+    margin: 5,
+    alignItems: 'center',
+    shadowOffset:{  width: 2,  height: 2,  },
+    shadowColor: '#1D3557',
+    shadowOpacity: .4
+  },
+  listContainer: {
+    width: '80%',
+    padding: 10,
+    backgroundColor: '#FFFFFF'
+  },
+  heading: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    margin: 10,
+    color: '#1D3557'
+  },
+  info: {
+    fontSize: 14,
+    color: '#1D3557'
+  },
+  name: {
+    fontSize: 18,
+    color: '#1D3557',
+    fontWeight: 'bold'
+  }
+})
